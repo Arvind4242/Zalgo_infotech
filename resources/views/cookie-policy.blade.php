68e94aea246cac1c33b1aa08fab73997819b7fe2
Arvind4242 <arvindsinghsikarwar606@gmail.com>
2026-02-16 19:09:20 +0530

    cookies pop-up

---
+++ resources/views/cookie-policy.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container py-5">
+<h1>Cookie Policy</h1>
+
+<p>This website uses cookies to enhance user experience.</p>
+
+<h3>Types of Cookies</h3>
+<ul>
+<li>Essential cookies</li>
+<li>Analytics cookies</li>
+<li>Marketing cookies</li>
+</ul>
+
+<p>By using our site you consent to cookies.</p>
+</div>
+@endsection
